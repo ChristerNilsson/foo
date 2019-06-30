2af42ad1e66ba60914d40ffc36e834cc810f2f4a
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-06-30 17:27:43 +0200

    x

--- 013-JS2W1/incrementSimple.js
+++ 013-JS2W1/incrementSimple.js
@@ -1,6 +1,7 @@
 "use strict"
 
-const update = () => {
+
+function update () {
 	let value = parseInt(number.innerText)
 	value++
 	number.innerText = value
@@ -8,18 +9,29 @@ const update = () => {
 
 const root = document.getElementById('root')
 
-const number = document.createElement('div')
-number.style.width = '100%'
-number.innerText = 0
-number.style.textAlign = 'center'
-number.style.fontSize = '250%'
-root.appendChild(number)
+const makeDiv = (title) => {
+	const number = document.createElement('div')
+	number.style.width = '50%'
+	number.innerText = title
+	number.style.float = 'left'
+	number.style.textAlign = 'center'
+	number.style.fontSize = '250%'
+	root.appendChild(number)	
+	return number
+}
 
-const incr = document.createElement('button')
-incr.onclick = update
-incr.setAttribute('class', 'my-button')
-incr.style.width = '100%'
-incr.innerText = 'incr'
-incr.style.textAlign = 'center'
-incr.style.fontSize = '250%'
-root.appendChild(incr)
+function makeButton(title,click) {
+	const button = document.createElement('button')
+	button.onclick = click
+	button.setAttribute('class', 'my-button')
+	button.style.width = '50%'
+	button.innerText = title
+	button.style.float = 'left'
+	button.style.textAlign = 'center'
+	button.style.fontSize = '250%'
+	root.appendChild(button)
+	return button
+}
+
+const number = makeDiv('10')
+const incr = makeButton('incr',update)
