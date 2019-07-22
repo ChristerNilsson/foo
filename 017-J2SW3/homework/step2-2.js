7114713ba5a7b1960f06f24ece48d1afa7c608c3
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-07-22 18:18:26 +0200

    x

--- 017-J2SW3/homework/step2-2.js
+++ 017-J2SW3/homework/step2-2.js
@@ -4,7 +4,7 @@ const assert = require('./assert').assert
 function threeFive(startIndex, stopIndex, call3, call5) {
 	for (let i=startIndex; i<=stopIndex; i++) {
 		if (i%3 == 0) call3(i)
-		if (i%5 == 0) call5(i,i)
+		if (i%5 == 0) call5(i,i*i)
 	}
 }
 
@@ -18,4 +18,4 @@ const m5 = createMock()
 threeFive(10, 15, m3.mock, m5.mock)
 
 assert(m3.args(), [[12],[15]])
-assert(m5.args(), [[10,10],[15,15]])
+assert(m5.args(), [[10,100],[15,225]])
