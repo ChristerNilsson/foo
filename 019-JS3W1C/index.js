8e9f18d17a42c4bf50e1e74a828a0d23920887b0
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-07-24 01:04:13 +0200

    x

---
+++ 019-JS3W1C/index.js
@@ -0,0 +1,48 @@
+const assert = chai.assert.deepStrictEqual
+
+function fetch(url, callback)	{
+	console.log('fetch',url)
+	const req = new XMLHttpRequest
+	req.open("GET",url)
+	req.responseType = "json"
+	req.onload = () => callback(req.response)
+	req.send()
+}
+
+fetch('repos.json', f)
+
+function f (repos) {
+	console.log('f',repos.length)
+	assert(repos.length, 2)
+
+	assert(repos[0].name, 'AngularJS')
+	assert(repos[0].description, 'Class material from angularJS module')
+	assert(repos[0].forks, 2)
+	assert(repos[0].updated_at, "2018-04-06T17:18:06Z")
+	assert(repos[0].html_url, "https://github.com/HackYourFuture/AngularJS")
+	assert(repos[0].contributors_url, "https://api.github.com/repos/HackYourFuture/AngularJS/contributors")
+
+	assert(repos[1].name, 'tdd-game')
+}
+
+fetch('AngularJS/contributors.json', g)
+
+function g (contributors) {
+	console.log('g',contributors.length)
+	assert(contributors[0].login, 'isalga')
+	assert(contributors[0].html_url, "https://github.com/isalga")
+	assert(contributors[0].avatar_url, "https://avatars3.githubusercontent.com/u/3985124?v=4")
+	assert(contributors[0].contributions, 24)
+}
+
+fetch('tdd-game/contributors.json', h)
+
+function h (contributors) {
+	console.log('h',contributors.length)
+	assert(contributors[0].login, 'M3kH')
+}
+
+// fetch('https://api.github.com/orgs/HackYourFuture/repos?per_page=100',k)
+// function k (repos) {
+// 	console.log('k',repos.length)
+// }
