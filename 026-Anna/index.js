96016c1d335003bb0c341f558731a56f2626508c
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-08-14 17:15:59 +0200

    x

--- 026-Anna/index.js
+++ 026-Anna/index.js
@@ -5,57 +5,41 @@ const range = _.range
 const print = console.log
 
 const GRAVITY = 0.1
-const balls = []
+let balls = []
 
 class Ball  {
-	constructor(x,y,radius,vx,vy,r,g,b) {
-		this.x = x
-    this.y = y
-    this.radius = radius
-    this.vx = vx
-    this.vy = vy
-    this.r = r
-    this.g = g
-		this.b = b
-		this.active = true
+	constructor (x,y,radius,vx,vy,r,g,b) {
+		_.extend(this,{x,y,radius,vx,vy,r,g,b})
 	}
 
-	inside (mx,my) {
-		return dist(mx,my,this.x,this.y) < this.radius
-	}
+	inside = (mx,my) => dist(mx,my,this.x,this.y) < this.radius
 
 	update () {
 		this.x += this.vx
-		if (this.x + this.radius > width || this.x < this.radius) {
-			this.vx = -this.vx
-		}
-	
 		this.y += this.vy
-		if (this.y + this.radius > height) {
-			this.vy = -this.vy
-		} else {
-			this.vy += GRAVITY	
-		}
+		if (this.x + this.radius > width || this.x < this.radius) this.vx = -this.vx	
+		if (this.y + this.radius > height) this.vy = -this.vy; else this.vy += GRAVITY	
 	}
 
 	draw () {
 		fill(this.r, this.g, this.b)
 		ellipse(this.x, this.y,2*this.radius,2*this.radius)
+		this.update()
 	}
 }
 
 function setup () {
 	createCanvas(windowWidth,windowHeight)
 	ellipseMode(CENTER)
-	for (const i in range(10)) {
+	for (const i of range(10)) {
 		const x = width*random()
 		const y = 100*random()
 		const radius = 50 + 50*random()
-		const vx = 2*random()
-		const vy = 3*random()
-		const r = 255*random()
-		const g = 255*random()
-		const b = 255*random()
+		const vx = 1+2*random()
+		const vy = 1+3*random()
+		const r = 256*random()
+		const g = 256*random()
+		const b = 256*random()
 		const ball = new Ball(x,y,radius,vx,vy,r,g,b)
 		balls.push(ball)
 	}
@@ -63,19 +47,8 @@ function setup () {
 
 function draw () {
 	background(128)
-	for (const ball of balls) {
-		if (ball.active) {
-			ball.draw()
-			ball.update()	
-		}
-	}
+	balls.forEach(ball => ball.draw())
 }
 
-function mousePressed () {
-	for (const ball of balls) {
-		if (ball.inside(mouseX,mouseY)) {
-			ball.active = false
-		}
-	}
-}
+const mousePressed = () => balls = balls.filter(ball => ! ball.inside(mouseX,mouseY))
 	
