1abc4fb5787b7cf8a109004e34e4187e36bc2767
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-08-28 12:13:08 +0200

    x

--- 040-LiveShare-Nim/src/nim.js
+++ 040-LiveShare-Nim/src/nim.js
@@ -5,24 +5,60 @@ const range = _.range
 const chai = require('chai')
 const assert = chai.assert.deepEqual
 
-class Nim {}
+class Nim {
+	constructor (){
+    this.state = [1,3,5]
+	}
+  toString (){
+    return JSON.stringify(this.state)
+  }
+  human (heap, sticks) {
+    this.state[heap] -= sticks
+  }
+  computer () {
+		const [a,b,c] = this.state
+		const total = a^b^c
+		if (total == 0) {
+			// losing                       0 1 2
+			for (const i of range(3)) {
+        if (this.state[i]>0) {
+					this.state[i]--
+					break
+				}
+			}
+		} else {
+			// winning
+			for (const i of range(3)) {
+				if ((this.state[i] ^ total) < this.state[i]) {
+					this.state[i] = this.state[i] ^ total
+					break
+				}
+			}
+		}
+		
+  }
+  reset () {
+    this.state = [1,3,5]
+  }
+
+}
 
 const nim = new Nim
-// assert(nim.state, [1,3,5])
-// assert(nim.toString(), '[1,3,5]')
-// nim.human(2,3)
-// assert(nim.state, [1,3,2])
-// nim.computer()
-// assert(nim.state, [0,3,2])
-// nim.human(1,2)
-// assert(nim.state, [0,1,2])
-// nim.computer()
-// assert(nim.state, [0,1,1])
-// nim.human(1,1)
-// assert(nim.state, [0,0,1])
-// nim.computer()
-// assert(nim.state, [0,0,0])	
-// nim.reset()
-// assert(nim.state, [1,3,5])
+assert(nim.state, [1,3,5])
+assert(nim.toString(), '[1,3,5]')
+nim.human(2,3)
+assert(nim.state, [1,3,2])
+nim.computer()
+assert(nim.state, [0,3,2]) 
+nim.human(1,2)
+assert(nim.state, [0,1,2])
+nim.computer()
+assert(nim.state, [0,1,1])
+nim.human(1,1)
+assert(nim.state, [0,0,1])
+nim.computer()
+assert(nim.state, [0,0,0])	
+nim.reset()
+assert(nim.state, [1,3,5])
 
 module.exports = Nim
--- 040-LiveShare-Nim/src/server.js
+++ 040-LiveShare-Nim/src/server.js
@@ -1,14 +1,41 @@
 'use strict'
 
 const http = require('http')
-//const Nim = require('./nim')
+const Nim = require('./nim')
 
 function createServer(port) {
+	const nim = new Nim()
   return http.createServer((req, res) => {
-		console.log(req.url)
-		res.writeHead(200,{'Content-Type': 'application/json'})
-		res.end('Time to work!')
+		const arr = req.url.split('/')
+		console.log(arr)
+		if (arr.length == 3) {
+			const heap = arr[1]
+			const sticks = arr[2]
+			res.writeHead(200,{'Content-Type': 'application/json'})
+			nim.human('abc'.indexOf(heap),sticks)
+			res.write(`Human: ${nim}`)
+			nim.computer()
+			res.end(`Computer: ${nim}`)
+		} else if (arr.length == 2) {
+			if (arr[1] == 'reset') {
+				nim.reset()
+				res.writeHead(200,{'Content-Type': 'application/json'})
+				res.end(`${nim}`)
+			}
+		}
 	})
 }
 
 module.exports = createServer
+
+// localhost:3000
+// state = 10
+// localhost:3000/add
+// state = 11
+// localhost:3000/add
+// state = 12
+// localhost:3000/sub
+// state = 11
+// localhost:3000/reset
+// state = 10
+
