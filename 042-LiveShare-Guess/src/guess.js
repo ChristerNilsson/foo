46b2b145fdf9c0e34fdf8a888097c1e50a5e04fa
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-08-29 11:14:47 +0200

    x

--- 042-LiveShare-Guess/src/guess.js
+++ 042-LiveShare-Guess/src/guess.js
@@ -8,17 +8,17 @@ class Guess {}
 
 const guess = new Guess()
 guess.number = 82
-// assert(`${guess}`,'Your guesses are []')
+// assert(guess.history, [])
 // assert(guess.guess(50),'Low!')
-// assert(guess.history,[50])
-// assert(guess.guess(75),'Low!')
-// assert(guess.guess(87),'High!')
-// assert(guess.guess(81),'Low!')
-// assert(guess.guess(84),'High!')
-// assert(guess.guess(82),'Correct!')
-// assert(guess.history,[50,75,87,81,84,82])
-// assert(`${guess}`,'Your guesses are [50,75,87,81,84,82]')
+// assert(guess.history, [50])
+// assert(guess.guess(75), 'Low!')
+// assert(guess.guess(87), 'High!')
+// assert(guess.guess(81), 'Low!')
+// assert(guess.guess(84), 'High!')
+// assert(guess.guess(82), 'Correct!')
+// assert(guess.history, [50,75,87,81,84,82])
+// assert(guess.toString(), '[50,75,87,81,84,82]')
 // guess.reset()
-// assert(guess.history,[])
+// assert(guess.history, [])
 
 module.exports = Guess
